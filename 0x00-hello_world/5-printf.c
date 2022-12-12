6545d835f133170c3becfde97448c7e44a29a766
chienyiokey <chienyiok@gmail.com>
2022-12-12 03:10:01 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a line with printf
+ *
+ * Return: 0 always success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
