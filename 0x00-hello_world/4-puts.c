d0856378503d56df279f4211165407350725e4c5
chienyiokey <chienyiok@gmail.com>
2022-12-12 02:54:45 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - print a line with puts
  *
  * Return: Always 0 success
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
