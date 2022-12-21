57e87c2d385ed424e647f499651f78816c0c90b4
chienyiokey <chienyiok@gmail.com>
2022-12-21 01:25:12 -0800

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pionter as an int to parameter and update the value
+ * @n: pointer to an int
+ * Return: alway 0 success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef _OKEY_MAIN_H
+#define _OKEY_MAIN_H
+void reset_to_98(int *n);
+
+#endif
